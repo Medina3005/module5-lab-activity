50001ea009d59be8b07ad76aeaaa331434a7a121
Medina3005 <114082560+Medina3005@users.noreply.github.com>
2025-02-12 20:43:40 -0600

    Add files via upload
    Problem 1: Print "Hello World" 100 times

---
+++ print_hello.py
@@ -0,0 +1,7 @@
+# Problem 1: Print "Hello World" 100 times
+# by Medina Kubanychbekova
+# Date: 02/12/2025
+# Description: This program prints "Hello World" 100 times using a loop.
+
+for i in range(100):
+    print("Hello World")
